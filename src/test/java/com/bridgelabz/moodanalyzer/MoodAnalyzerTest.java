91fc6dc95ad86851bdbab559ee45f7eb643f1254
S MD ASJAD AZMI <asjadazmi0786@gmail.com>
2022-06-23 11:26:39 +0530

    Added : Testing for the sad mood

--- src/test/java/com/bridgelabz/moodanalyzer/MoodAnalyzerTest.java
+++ src/test/java/com/bridgelabz/moodanalyzer/MoodAnalyzerTest.java
@@ -1,19 +1,17 @@
 package com.bridgelabz.moodanalyzer;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 public class MoodAnalyzerTest {
+
     @Test
     public void sadMessage() {
         MoodAnalyzer moodAnalyser = new MoodAnalyzer("I'm In Sad Mood");
         String mood = moodAnalyser.analyseMood();
         Assertions.assertEquals("Sad", mood);
     }
-    @Test
-    public void happyMessage() {
-        MoodAnalyzer moodAnalyser = new MoodAnalyzer("I'm In Happy Mood");
-        String mood = moodAnalyser.analyseMood();
-        Assertions.assertEquals("Happy", mood);
-    }
+
+//
 }
